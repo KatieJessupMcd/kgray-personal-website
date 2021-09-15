c6d02311e8efbe06bdea37b247ed959c4502a08a
KatieJessupMcD <katie.jessupmcd@gmail.com>
2021-09-14 20:54:50 -0700

    implementing navigation bar

--- src/components/header.js
+++ src/components/header.js
@@ -5,29 +5,69 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `green`,
+      background: `#5496B6`,
       marginBottom: `0`,
+      color: `white`,
+      fontFamily: `Roboto`,
     }}
   >
     {/* inline styling for header */}
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        maxWidth: 690,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <nav className="nav-container">
+        <div className="first-name">Katie</div>
+        <div className="last-name">Gray</div>
+        <div className="home-link">
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            Home
+          </Link>
+        </div>
+        <div className="about-link">
+          <Link
+            to="/about"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            About
+          </Link>
+        </div>
+        <div className="work-link">
+          <Link
+            to="/work"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            Work
+          </Link>
+        </div>
+        <div className="blog-link">
+          <Link
+            to="/blog"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            Blog
+          </Link>
+        </div>
+        <div className="horizontal-line"></div>
+      </nav>
     </div>
   </header>
 )
