59784cf13023c324d33cbc2d87fa48633dbeaa38
KatieJessupMcD <katie.jessupmcd@gmail.com>
2021-09-15 20:54:19 -0700

    implementing overlapping images with css grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,23 +8,45 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi, I'm Katie!</h1>
-    <p>
-      I'm a software engineer with an eye for detail and growth mindset. Stay a
-      while!
-    </p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <section>
+      <ul
+        style={{
+          display: `grid`,
+          gridTemplateColumns: `4fr 2fr 2fr 2fr`,
+          maxWidth: `400px`,
+          listStyle: `none`,
+        }}
+      >
+        <li
+          style={{ gridRow: `1 / span 2`, gridColumn: `1 / span 2`, zIndex: 2 }}
+        >
+          <StaticImage
+            src="../images/portrait-sketch.jpeg"
+            width={270}
+            height={270}
+            quality={95}
+            formats={["auto", "webp", "avif"]}
+            alt="A Gatsby astronaut"
+            style={{
+              marginBottom: `1.45rem`,
+              borderRadius: `180px`,
+              boxShadow: `12px 12px 2px 1px rgba(0, 0, 255, .2)`,
+            }}
+          />
+        </li>
+        <li style={{ gridRow: `2 / span 4`, gridColumn: `2 / span 3` }}>
+          <StaticImage
+            src="../images/tea-sketch.jpeg"
+            width={270}
+            height={270}
+            quality={95}
+            formats={["auto", "webp", "avif"]}
+            alt="A Gatsby astronaut"
+            style={{ marginBottom: `1.45rem`, borderRadius: `180px` }}
+          />
+        </li>
+      </ul>
+    </section>
   </Layout>
 )
 
